f511ffcfc36f75614f98570010a863ee887a6fcd
Geert Theys <geert.theys@gmail.com>
2014-07-07 11:56:23 +0200

    pretify

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,22 +2,37 @@
 module.exports = function(grunt) {
 
   // Project configuration.
-    grunt.initConfig({
+  grunt.initConfig({
 
-        copy: {
-            main: {
-                files: [
-                    { expand: true, cwd: "assets/bootstrap/dist/css/", src: 'bootstrap.css', dest: "static/bootstrap/css/" },
-                    { expand: true, cwd: "assets/bootstrap/dist/fonts/", src: '**',dest: "static/bootstrap/fonts" },
-                    { expand: true, cwd: "assets/bootstrap/dist/js/", src: '*.min.js', dest: "static/bootstrap/js" },
-                    { expand: true, cwd: "assets/jquery/dist/", src: '*.min.js', dest: "static/jquery" }
-                ]
-            }
-        },
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: "assets/bootstrap/dist/css/",
+          src: 'bootstrap.css',
+          dest: "static/bootstrap/css/"
+        }, {
+          expand: true,
+          cwd: "assets/bootstrap/dist/fonts/",
+          src: '**',
+          dest: "static/bootstrap/fonts"
+        }, {
+          expand: true,
+          cwd: "assets/bootstrap/dist/js/",
+          src: '*.min.js',
+          dest: "static/bootstrap/js"
+        }, {
+          expand: true,
+          cwd: "assets/jquery/dist/",
+          src: '*.min.js',
+          dest: "static/jquery"
+        }]
+      }
+    },
 
-        tasks: ['copy'],
+    tasks: ['copy'],
 
-    });
+  });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
 
