b76ca08479002f94c877fcb1be4acad1c5b15406
Geert Theys <geert.theys@gmail.com>
2014-07-13 12:44:58 +0200

    Resoled issues:
    * Bad bower.json
    * wrong includes in the header

--- content/javascript/all.js
+++ content/javascript/all.js
@@ -1,1 +1,29 @@
+// Carousel functionality
 $('#myCarousel').carousel();
+
+// Contact form submission
+$(document).ready(function() {
+
+    $('#contactForm').submit(function() {
+
+        $.ajax({
+            type: "POST",
+            url: "http://cooldevops_contact.dev/contact/",
+            data: $("#contactForm").serialize(),
+            dataType: "json",
+
+            success: function(msg) {
+                $("#formResponse").removeClass('error');
+                $("#formResponse").removeClass('success');
+                $("#formResponse").addClass(msg.status);
+                $("#formResponse").html(msg.message);
+
+            },
+            error: function() {
+                $("#formResponse").removeClass('success');
+                $("#formResponse").addClass('error');
+                $("#formResponse").html("There was an error submitting the form. Please try again.");
+            }
+        });
+    });
+});
